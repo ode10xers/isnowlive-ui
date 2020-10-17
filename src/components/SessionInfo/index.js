f2008f2ca66952b4dc0dd9e703f71c0ca932b087
SanketKarve <sanketkarve43@gmail.com>
2020-10-17 16:23:58 +0530

    made review changes

--- src/components/SessionInfo/index.js
+++ src/components/SessionInfo/index.js
@@ -10,23 +10,25 @@ const SessionInfo = ({ session }) => {
   return (
     <Row justify="space-between">
       <Col xs={8} md={8}>
-        <Text type="secondary">Session Type</Text> <br />
-        <Text className={styles.subText} strong>
-          {session?.group ? 'Group session' : '1-on-1 Session'}
+        <Text className={styles.text} type="secondary">
+          Session Type
         </Text>
+        <Text className={styles.subText}>{session?.group ? 'Group session' : '1-on-1 Session'}</Text>
       </Col>
       <Col xs={8} md={8}>
-        <Text type="secondary">Price</Text>
-        <br />
-        <Text className={styles.subText} strong>
+        <Text className={styles.text} type="secondary">
+          Price
+        </Text>
+        <Text className={styles.subText}>
           {session?.price || 0} {session?.currency}
         </Text>
       </Col>
       <Col xs={8} md={8}>
-        <Text type="secondary">Session Prerequisite</Text>
-        <br />
-        <Text className={styles.subText} strong>
-          <Button className={styles.button} type="link" icon={<FilePdfOutlined />} size="middle">
+        <Text className={styles.text} type="secondary">
+          Session Prerequisite
+        </Text>
+        <Text className={styles.subText}>
+          <Button className={styles.downloadButton} type="link" icon={<FilePdfOutlined />} size="middle">
             Download
           </Button>
         </Text>
