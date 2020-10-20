a8a50db4fe51801c41b212025e0dfb98478dc29a
Gopal Yadav <yadav.gopal43@gmail.com>
2020-10-20 16:57:37 +0530

    Removed comments.

--- src/components/SideNavigation/index.js
+++ src/components/SideNavigation/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Menu } from 'antd';
 
 import menuItems from './MenuItems.constant';
 
 import styles from './style.module.scss';
-import { useHistory } from 'react-router-dom';
 
 const { SubMenu } = Menu;
-
-// TODO: In Below component, we need to add an action which can be triggered on the click of the menu item.
-// And that action
 
 const SideNavigation = () => {
   const history = useHistory();
