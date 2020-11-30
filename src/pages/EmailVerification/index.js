4fce4b4d2cda83a14d708e09e8c9bae1a5b1c482
Gopal Yadav <yadav.gopal43@gmail.com>
2020-12-01 01:51:48 +0530

    error handling

--- src/pages/EmailVerification/index.js
+++ src/pages/EmailVerification/index.js
@@ -1,31 +1,27 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 
-import { Row } from 'antd';
+import { Row, message } from 'antd';
 import Loader from 'components/Loader';
-
 import apis from 'apis';
 import Routes from 'routes';
 import { isAPISuccess } from 'utils/helper';
 
 export default function EmailVerification() {
-
   const history = useHistory();
   const location = useLocation();
   const token = location.pathname.replace('/email/verify/', '');
   const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
 
   const verifyUserEmail = useCallback(async () => {
     try {
       setIsLoading(true);
-      setIsError(false);
-      const { data, status } = await apis.user.verifyEmail({ token });
+      const { status } = await apis.user.verifyEmail({ token });
       if (isAPISuccess(status)) {
-        history.push(Routes.createPassword, { token: data.auth_token });
+        history.push(Routes.createPassword, { token: token });
       }
     } catch (error) {
-      
+      message.error(error.response?.data?.message || 'Something went wrong.');
     }
   }, [token, history]);
 
@@ -33,11 +29,11 @@ export default function EmailVerification() {
     if (token) {
       verifyUserEmail();
     }
-  }, [token, verifyUserEmail])
+  }, [token, verifyUserEmail]);
 
   return (
     <Row justify="center" align="center">
       <Loader loading={isLoading} size="large" text="Verifying email" />
     </Row>
-  )
+  );
 }
