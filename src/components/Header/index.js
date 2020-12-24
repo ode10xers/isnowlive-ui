a44e74a981f8dca1f6e80248bc4c9aa216f20f9f
Ellianto <ellianto@10xers.co>
2020-12-24 19:25:53 +0700

    Removed unused parts and console logs + Refactor JSX Components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -34,28 +34,25 @@ const Header = () => {
       trackAndNavigate(Routes.creatorDashboard.rootPath, user.click.switchToCreator);
     } else {
       Modal.confirm({
-        className: styles.confirmModal,
         autoFocusButton: 'cancel',
         centered: true,
         closable: true,
         closeIcon: <CloseOutlined />,
         maskClosable: true,
         content: (
-          <Paragraph>
-            Ready to become a host and start making money by hosting live events?
-            <br /> <br />
-            By clicking on "<strong>Become Host</strong>" your account will be upgraded to a host account and you will
-            get access to your dashboard and features empowering you to host live events on topics you are passionate
-            about and make money from it.
-          </Paragraph>
+          <>
+            <Paragraph>Ready to become a host and start making money by hosting live events?</Paragraph>
+            <Paragraph>
+              By clicking on "<strong>Become Host</strong>" your account will be upgraded to a host account and you will
+              get access to your dashboard and features empowering you to host live events on topics you are passionate
+              about and make money from it.
+            </Paragraph>
+          </>
         ),
         title: 'Become a Host',
         okText: 'Become Host',
         cancelText: 'Talk to Us',
-        onOk: () => {
-          console.log('OK Clicked');
-          trackAndNavigate(Routes.profile, attendee.click.dashboard.becomeHost);
-        },
+        onOk: () => trackAndNavigate(Routes.profile, attendee.click.dashboard.becomeHost),
         onCancel: () => openFreshChatWidget(),
       });
     }
