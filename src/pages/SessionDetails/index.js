44943713cacfcb8ddb280a9a1b116fd5ab90279c
Ellianto <ellianto@10xers.co>
2021-01-31 15:49:38 +0700

    Filtered max capacity inventories in session details page

--- src/pages/SessionDetails/index.js
+++ src/pages/SessionDetails/index.js
@@ -64,12 +64,18 @@ const SessionDetails = ({ match, history }) => {
         const sessionDetails = await apis.session.getSessionDetails(session_id);
         const userDetails = await apis.user.getProfileByUsername(username);
         const passes = await apis.passes.getPassesBySessionId(session_id);
-        setSession(sessionDetails.data);
+        setSession({
+          ...sessionDetails.data,
+          inventory: sessionDetails.data.inventory.filter(
+            (inventory) => inventory.num_participants < sessionDetails.data.max_participants
+          ),
+        });
         setCreator(userDetails.data);
         setAvailablePasses(passes.data);
         setIsLoading(false);
         const latestInventories = sessionDetails.data.inventory
           .filter((inventory) => isBeforeDate(inventory.end_time))
+          .filter((inventory) => inventory.num_participants < sessionDetails.data.max_participants)
           .sort((a, b) => (a.start_time > b.start_time ? 1 : b.start_time > a.start_time ? -1 : 0));
         setSelectedInventory(latestInventories.length > 0 ? latestInventories[0] : null);
       } catch (error) {
