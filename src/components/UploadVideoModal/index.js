92509ff5ee33212abb972eb490302ed28efe8832
Ellianto <ellianto@10xers.co>
2021-02-10 16:04:22 +0700

    Reverted back to 5 MB chunks

--- src/components/UploadVideoModal/index.js
+++ src/components/UploadVideoModal/index.js
@@ -70,7 +70,7 @@ const UploadVideoModal = ({
     endpoint: `${config.server.baseURL}/creator/videos/${editedVideo?.external_id}/upload`,
     resume: true,
     retryDelays: null,
-    chunkSize: 50 * 1024 * 1024, // Required a minimum chunk size of 5 MB, here we use 50 MB.
+    chunkSize: 5 * 1024 * 1024, // Required a minimum chunk size of 5 MB, here we use 50 MB.
   });
 
   uppy.current.on('file-added', (file) => {
