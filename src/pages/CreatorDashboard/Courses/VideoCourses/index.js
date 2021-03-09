444b7a96502ac434a232f5cc3893957688e5a617
Ellianto <ellianto@10xers.co>
2021-03-09 23:04:27 +0700

    Added coupon code info on courses' buyers list

--- src/pages/CreatorDashboard/Courses/VideoCourses/index.js
+++ src/pages/CreatorDashboard/Courses/VideoCourses/index.js
@@ -183,20 +183,27 @@ const VideoCourses = ({ videoCourses, showEditModal, publishCourse, unpublishCou
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
-      width: '30%',
     },
     {
       title: 'Date of Purchase',
       dataIndex: 'date_of_purchase',
       key: 'date_of_purchase',
-      width: '30%',
+      width: '170px',
       render: (text, record) => toDateAndTime(record.date_of_purchase),
     },
     {
       title: 'Net Price',
       dataIndex: 'price_paid',
       key: 'price_paid',
+      width: '100px',
       render: (text, record) => `${record.currency?.toUpperCase()} ${record.price_paid}`,
+    },
+    {
+      title: 'Discount Code',
+      dataIndex: 'discount',
+      key: 'discount',
+      width: '180px',
+      render: (text, record) => record.discount?.code || 'No Discount',
     },
   ];
 
@@ -212,21 +219,28 @@ const VideoCourses = ({ videoCourses, showEditModal, publishCourse, unpublishCou
     );
   };
 
-  const renderMobileSubscriberCards = (subscriber) => (
-    <Card key={subscriber.date_of_purchase}>
+  const renderMobileSubscriberCards = (subscriber) => {
+    const layout = (label, value) => (
       <Row>
-        <Col xs={24}>
-          <Title level={5}> {subscriber.name} </Title>
+        <Col span={8}>
+          <Text strong>{label}</Text>
         </Col>
-        <Col xs={24}>
-          <Text> Purchased at {toDateAndTime(subscriber.date_of_purchase)} </Text>
-        </Col>
-        <Col xs={24}>
-          <Text> {`${subscriber.price_paid} ${subscriber.currency.toUpperCase()}`} </Text>
-        </Col>
+        <Col span={16}>: {value}</Col>
       </Row>
-    </Card>
-  );
+    );
+
+    return (
+      <Card
+        key={subscriber.date_of_purchase}
+        bodyStyle={{ padding: '20px 10px' }}
+        title={<Title level={5}> {subscriber.name} </Title>}
+      >
+        {layout('Buy Date', toDateAndTime(subscriber.date_of_purchase))}
+        {layout('Price', <Text strong> {`${subscriber.price_paid} ${subscriber.currency.toUpperCase()}`} </Text>)}
+        {layout('Discount Code', <Text strong> {subscriber.discount?.code || 'No Discount'} </Text>)}
+      </Card>
+    );
+  };
 
   const renderCourseItem = (course) => {
     const layout = (label, value) => (
