73f14e552258fe805a39fc7299001b06feeed633
Ellianto <ellianto@10xers.co>
2021-03-16 23:36:09 +0700

    Refactor showcase subscription cards

--- src/components/CreatorSubscriptions/index.js
+++ src/components/CreatorSubscriptions/index.js
@@ -1,36 +1,23 @@
 import React, { useState } from 'react';
 
-import { Row, Col, Card, Typography, Button, Divider, List, Space, message } from 'antd';
+import { Row, Col, List, message } from 'antd';
 
 import Loader from 'components/Loader';
 import PurchaseModal from 'components/PurchaseModal';
 import { showErrorModal, showSuccessModal } from 'components/Modals/modals';
+import ShowcaseSubscriptionCards from 'components/ShowcaseSubscriptionCards';
 
 import dateUtil from 'utils/date';
+import { generateBaseCreditsText } from 'utils/subscriptions';
 
 import { useGlobalContext } from 'services/globalContext';
 
-import styles from './styles.module.scss';
 import apis from 'apis';
 import { isAPISuccess } from 'utils/helper';
 
 const {
   timezoneUtils: { getTimezoneLocation },
 } = dateUtil;
-
-const { Text } = Typography;
-const defaultBorderColor = '#f0f0f0';
-
-const productTextMapping = {
-  SESSION: 'Sessions',
-  VIDEO: 'Videos',
-  COURSE: 'Courses',
-};
-
-const accessTypeTextMapping = {
-  PUBLIC: 'Public',
-  MEMBERSHIP: 'Membership',
-};
 
 const CreatorSubscriptions = ({ subscriptions }) => {
   const { showPaymentPopup } = useGlobalContext();
@@ -54,8 +41,6 @@ const CreatorSubscriptions = ({ subscriptions }) => {
       showErrorModal('Something went wrong', 'Invalid Course Selected');
       return;
     }
-
-    console.log(selectedSubscription);
 
     let itemDescription = [];
 
@@ -121,126 +106,9 @@ const CreatorSubscriptions = ({ subscriptions }) => {
     setIsLoading(false);
   };
 
-  const generateBaseCreditsText = (subscription, isCourse = false) => {
-    let calculatedBaseCredits = 0;
-    let productText = '';
-
-    if (isCourse) {
-      calculatedBaseCredits = subscription?.products['COURSE']?.credits || 0;
-      productText = 'Course';
-    } else {
-      calculatedBaseCredits =
-        (subscription?.products['SESSION']?.credits || 0) + (subscription?.products['VIDEO']?.credits || 0);
-
-      let availableProducts = [];
-
-      if (subscription?.products['SESSION']) {
-        availableProducts.push('Sessions');
-      }
-
-      if (subscription?.products['VIDEO']) {
-        availableProducts.push('Videos');
-      }
-
-      productText = availableProducts.join(' or ');
-    }
-
-    return `${calculatedBaseCredits} ${productText} credits/month`;
-  };
-
-  const generateIncludedProducts = (subscription, isCourse = false) => {
-    let productTexts = [];
-
-    if (isCourse) {
-      productTexts =
-        subscription?.products['COURSE']?.access_types?.map(
-          (accessType) => `${accessTypeTextMapping[accessType]} ${productTextMapping['COURSE']}`
-        ) || [];
-    } else {
-      const excludedProductKeys = ['COURSE'];
-      Object.entries(subscription?.products).forEach(([key, val]) => {
-        if (!excludedProductKeys.includes(key)) {
-          productTexts = [
-            ...productTexts,
-            ...val.access_types.map((accessType) => `${accessTypeTextMapping[accessType]} ${productTextMapping[key]}`),
-          ];
-        }
-      });
-    }
-
-    return (
-      <Space size="small" direction="vertical" align="center" className={styles.includedProductsList}>
-        {productTexts.map((productText) => (
-          <Text strong> {productText} </Text>
-        ))}
-      </Space>
-    );
-  };
-
   const renderShowcaseSubscriptionCards = (subscription) => (
     <List.Item key={subscription?.external_id}>
-      <Card
-        hoverable={true}
-        style={{ border: `1px solid ${subscription?.color_code || '#ffffff'}` }}
-        headStyle={{ textAlign: 'center', borderBottom: `1px solid ${subscription?.color_code || defaultBorderColor}` }}
-        title={
-          <div className={styles.subscriptionNameWrapper}>
-            <Text strong> {subscription?.name} </Text>
-          </div>
-        }
-        bodyStyle={{ textAlign: 'center' }}
-      >
-        <Row gutter={[8, 8]} justify="center">
-          <Col xs={24} className={styles.includedProductsWrapper}>
-            <Row gutter={[8, 10]} justify="center">
-              <Col xs={24}>
-                <div className={styles.baseCreditsText}>{generateBaseCreditsText(subscription, false)}</div>
-              </Col>
-              <Col xs={24} className={styles.includeTextWrapper}>
-                <Text disabled> Includes access to </Text>
-              </Col>
-              <Col xs={24}>{generateIncludedProducts(subscription)}</Col>
-            </Row>
-          </Col>
-          <Col xs={24}>
-            <Divider type="horizontal" />
-          </Col>
-          <Col xs={24} className={styles.includedCoursesWrapper}>
-            <Row gutter={[8, 10]} justify="center">
-              <Col xs={24}>
-                <div className={styles.baseCreditsText}>{generateBaseCreditsText(subscription, true)}</div>
-              </Col>
-              <Col xs={24} className={styles.includeTextWrapper}>
-                {subscription?.products['COURSE']?.access_types?.length > 0 && (
-                  <Text disabled> Includes access to </Text>
-                )}
-              </Col>
-              <Col xs={24}>{generateIncludedProducts(subscription, true)}</Col>
-            </Row>
-          </Col>
-          <Col xs={24}>
-            <div className={styles.subscriptionPriceWrapper} style={{ color: subscription?.color_code || '#000000' }}>
-              <span className={styles.subscriptionPriceText}>
-                {' '}
-                {subscription?.currency?.toUpperCase()} {subscription?.price}{' '}
-              </span>
-              / month
-            </div>
-          </Col>
-          <Col xs={24} className={styles.buyButtonWrapper}>
-            <Button
-              type="primary"
-              style={{
-                background: subscription?.color_code || '#1890ff',
-                borderColor: subscription?.color_code || '#1890ff',
-              }}
-              onClick={() => openPurchaseModal(subscription)}
-            >
-              Buy Subscription
-            </Button>
-          </Col>
-        </Row>
-      </Card>
+      <ShowcaseSubscriptionCards subscription={subscription} openPurchaseModal={openPurchaseModal} />
     </List.Item>
   );
 
---
+++ src/components/ShowcaseSubscriptionCards/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import { Row, Col, Card, Typography, Button, Divider, Space } from 'antd';
+
+import { generateBaseCreditsText, generateIncludedProducts } from 'utils/subscriptions';
+
+import styles from './styles.module.scss';
+
+const { Text } = Typography;
+const defaultBorderColor = '#f0f0f0';
+
+const ShowcaseSubscriptionCards = ({ subscription, openPurchaseModal = () => {} }) => {
+  return (
+    <Card
+      hoverable={true}
+      style={{ border: `1px solid ${subscription?.color_code || '#ffffff'}` }}
+      headStyle={{ textAlign: 'center', borderBottom: `1px solid ${subscription?.color_code || defaultBorderColor}` }}
+      title={
+        <div className={styles.subscriptionNameWrapper}>
+          <Text strong> {subscription?.name} </Text>
+        </div>
+      }
+      bodyStyle={{ textAlign: 'center' }}
+    >
+      <Row gutter={[8, 8]} justify="center">
+        <Col xs={24} className={styles.includedProductsWrapper}>
+          <Row gutter={[8, 10]} justify="center">
+            <Col xs={24}>
+              <div className={styles.baseCreditsText}>{generateBaseCreditsText(subscription, false)}</div>
+            </Col>
+            <Col xs={24} className={styles.includeTextWrapper}>
+              <Text disabled> Includes access to </Text>
+            </Col>
+            <Col xs={24}>
+              <Space size="small" direction="vertical" align="center" className={styles.includedProductsList}>
+                {generateIncludedProducts(subscription).map((productText) => (
+                  <Text strong> {productText} </Text>
+                ))}
+              </Space>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24}>
+          <Divider type="horizontal" />
+        </Col>
+        <Col xs={24} className={styles.includedCoursesWrapper}>
+          <Row gutter={[8, 10]} justify="center">
+            <Col xs={24}>
+              <div className={styles.baseCreditsText}>{generateBaseCreditsText(subscription, true)}</div>
+            </Col>
+            <Col xs={24} className={styles.includeTextWrapper}>
+              {subscription?.products['COURSE']?.access_types?.length > 0 && <Text disabled> Includes access to </Text>}
+            </Col>
+            <Col xs={24}>{generateIncludedProducts(subscription, true)}</Col>
+          </Row>
+        </Col>
+        <Col xs={24}>
+          <div className={styles.subscriptionPriceWrapper} style={{ color: subscription?.color_code || '#000000' }}>
+            <span className={styles.subscriptionPriceText}>
+              {subscription?.currency?.toUpperCase()} {subscription?.price}{' '}
+            </span>
+            / month
+          </div>
+        </Col>
+        <Col xs={24} className={styles.buyButtonWrapper}>
+          <Button
+            type="primary"
+            style={{
+              background: subscription?.color_code || '#1890ff',
+              borderColor: subscription?.color_code || '#1890ff',
+            }}
+            onClick={() => openPurchaseModal(subscription)}
+          >
+            Buy Subscription
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default ShowcaseSubscriptionCards;
