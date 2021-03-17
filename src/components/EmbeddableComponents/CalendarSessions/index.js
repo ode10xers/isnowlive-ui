d2ffb1d07785e16d08e9ae5295765105c4c391ac
Ellianto <ellianto@10xers.co>
2021-03-17 17:03:31 +0700

    Lint fixes

--- src/components/EmbeddableComponents/CalendarSessions/index.js
+++ src/components/EmbeddableComponents/CalendarSessions/index.js
@@ -17,9 +17,9 @@ const {
 } = dateUtil;
 
 function generateLightColorHex() {
-  let color = "#";
+  let color = '#';
   for (let i = 0; i < 3; i++)
-    color += ("0" + Math.floor(((1 + Math.random()) * Math.pow(16, 2)) / 2).toString(16)).slice(-2);
+    color += ('0' + Math.floor(((1 + Math.random()) * Math.pow(16, 2)) / 2).toString(16)).slice(-2);
   return color;
 }
 
@@ -63,20 +63,26 @@ const CalendarSessions = () => {
   }, []);
 
   function Event({ event }) {
-
     const onBookClick = (e) => {
       e.stopPropagation();
 
       alert('clicked event with id ' + event.inventory_id);
-    }
+    };
 
     const borderColor = generateLightColorHex();
 
     if (calendarView === 'month' || calendarView === 'agenda') {
       return (
-        <div style={{ fontSize: '12px', padding: '2px 4px 0', border: `2px solid ${borderColor}`, borderRadius: '3px' }}>
+        <div
+          style={{ fontSize: '12px', padding: '2px 4px 0', border: `2px solid ${borderColor}`, borderRadius: '3px' }}
+        >
           <span
-            style={{ display: 'inline-block', maxWidth: 'calc(100% - 60px)', overflow: 'hidden', textOverflow: 'ellipsis' }}
+            style={{
+              display: 'inline-block',
+              maxWidth: 'calc(100% - 60px)',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+            }}
           >
             {event.name}
           </span>
@@ -87,7 +93,16 @@ const CalendarSessions = () => {
 
     if (calendarView === 'week' || calendarView === 'day') {
       return (
-        <div style={{ height: '118px', fontSize: '12px', padding: '2px 4px 0', border: `2px solid ${borderColor}`, borderRadius: '3px', fontFamily: 'Roboto' }}>
+        <div
+          style={{
+            height: '118px',
+            fontSize: '12px',
+            padding: '2px 4px 0',
+            border: `2px solid ${borderColor}`,
+            borderRadius: '3px',
+            fontFamily: 'Roboto',
+          }}
+        >
           <div style={{ fontSize: '14px', margin: '5px 0 8px', color: '#595959', lineHeight: '16px' }}>
             {event.name}
           </div>
@@ -95,15 +110,32 @@ const CalendarSessions = () => {
             {`${toLocaleTime(event.start_time)} - ${toLocaleTime(event.end_time)}`}
           </div>
           <div style={{ marginBottom: '10px' }}>
-            {event.group &&
+            {event.group && (
               <span
-                style={{ display: 'inline-block', padding: '5px', background: '#B5F5EC', color: '#006D75', borderRadius: '4px' }}>
+                style={{
+                  display: 'inline-block',
+                  padding: '5px',
+                  background: '#B5F5EC',
+                  color: '#006D75',
+                  borderRadius: '4px',
+                }}
+              >
                 Group
-              </span>}
+              </span>
+            )}
           </div>
           <button
             onClick={(e) => onBookClick(e)}
-            style={{ width: '100%', textAlign: 'center', padding: '5px', background: '#FFFFFF', color: '#595959', border: '1px solid #F0F0F0', borderRadius: '2px' }}>
+            style={{
+              width: '100%',
+              textAlign: 'center',
+              padding: '5px',
+              background: '#FFFFFF',
+              color: '#595959',
+              border: '1px solid #F0F0F0',
+              borderRadius: '2px',
+            }}
+          >
             Book
           </button>
         </div>
@@ -126,8 +158,8 @@ const CalendarSessions = () => {
           step={30}
         />
       ) : (
-          <Empty />
-        )}
+        <Empty />
+      )}
     </Loader>
   );
 };
