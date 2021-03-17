567a4e03345ee01d1cb6932936de80d4cb9dcfff
Ellianto <ellianto@10xers.co>
2021-03-17 19:49:11 +0700

    Implement Send Email Modals

---
+++ src/components/SendCustomerEmailModal/index.js
@@ -0,0 +1,189 @@
+import React, { useState, useEffect } from 'react';
+
+import { Row, Col, Form, Modal, Tooltip, Input, Select, Typography, Button } from 'antd';
+import { FilePdfOutlined, CloseCircleOutlined } from '@ant-design/icons';
+
+import apis from 'apis';
+
+import FileUpload from 'components/FileUpload';
+import TextEditor from 'components/TextEditor';
+import { showErrorModal, showSuccessModal } from 'components/Modals/modals';
+
+import dateUtil from 'utils/date';
+import validationRules from 'utils/validation';
+import { isAPISuccess } from 'utils/helper';
+
+import { sendCustomerEmailFormLayout, sendCustomerEmailBodyFormLayout } from 'layouts/FormLayouts';
+
+import styles from './styles.module.scss';
+
+const { Title } = Typography;
+const {
+  timeCalculation: { isBeforeDate },
+} = dateUtil;
+
+const SendCustomerEmailModal = ({ visible, closeModal, recipients, productId, productType }) => {
+  const [form] = Form.useForm();
+
+  const [submitting, setSubmitting] = useState(false);
+  const [filteredRecipients, setFilteredRecipients] = useState([]);
+  const [selectedRecipients, setSelectedRecipients] = useState([]);
+  const [emailDocumentUrl, setEmailDocumentUrl] = useState(null);
+
+  useEffect(() => {
+    if (!visible || recipients?.length < 1) {
+      setSubmitting(false);
+      setFilteredRecipients([]);
+      setSelectedRecipients([]);
+      setEmailDocumentUrl(null);
+    } else {
+      const validRecipients = recipients?.filter((user) => isBeforeDate(user.expiry_date)) || [];
+      setFilteredRecipients(validRecipients);
+      setSelectedRecipients(validRecipients.map((user) => user.external_id));
+    }
+  }, [visible, recipients]);
+
+  const normFile = (e) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
+
+  const handleFinish = async (values) => {
+    setSubmitting(true);
+
+    try {
+      const payload = {
+        product_id: productId,
+        product_type: productType,
+        body: values.emailBody,
+        subject: values.subject,
+        user_ids: selectedRecipients || values.recipients,
+        document_url: emailDocumentUrl || '',
+      };
+
+      const { status } = await apis.user.sendProductEmailToCustomers(payload);
+
+      if (isAPISuccess(status)) {
+        showSuccessModal('Emails sent');
+        closeModal();
+      }
+    } catch (error) {
+      showErrorModal('Failed to send emails', error?.respoonse?.data?.message || 'Something went wrong');
+    }
+
+    setSubmitting(false);
+  };
+
+  return (
+    <Modal
+      title={<Title level={5}> Send email to customers </Title>}
+      visible={visible}
+      centered={true}
+      onCancel={() => closeModal()}
+      footer={null}
+      width={640}
+    >
+      <Form layout="horizontal" name="EmailForm" form={form} onFinish={handleFinish} scrollToFirstError={true}>
+        <Row gutter={[8, 16]}>
+          <Col xs={24}>
+            <Form.Item
+              {...sendCustomerEmailFormLayout}
+              id="recipients"
+              name="recipients"
+              label="Recipients"
+              rules={validationRules.arrayValidation}
+            >
+              <Select
+                showArrow
+                showSearch
+                placeholder="Select the recipients"
+                mode="multiple"
+                maxTagCount="responsive"
+                values={selectedRecipients}
+                options={filteredRecipients.map((user) => ({
+                  label: user.name,
+                  value: user.external_id,
+                }))}
+              />
+            </Form.Item>
+          </Col>
+          <Col xs={24}>
+            <Form.Item
+              {...sendCustomerEmailFormLayout}
+              id="subject"
+              name="subject"
+              label="Email Subject"
+              rules={validationRules.requiredValidation}
+            >
+              <Input placeholder="Subject of the email goes here" />
+            </Form.Item>
+          </Col>
+          <Col xs={24}>
+            <Form.Item
+              {...sendCustomerEmailBodyFormLayout}
+              label="Email Body"
+              name="emailBody"
+              id="emailBody"
+              rules={validationRules.requiredValidation}
+            >
+              <TextEditor name="emailBody" form={form} placeholder="Content of the email goes here" />
+            </Form.Item>
+          </Col>
+          <Col xs={24}>
+            <Form.Item name="document_url" id="document_url" valuePropName="fileList" getValueFromEvent={normFile}>
+              <Row>
+                <Col>
+                  <FileUpload
+                    name="document_url"
+                    value={emailDocumentUrl}
+                    onChange={setEmailDocumentUrl}
+                    listType="text"
+                    label="Upload a PDF file"
+                  />
+                </Col>
+                {emailDocumentUrl && (
+                  <Col>
+                    <Button
+                      type="text"
+                      icon={<FilePdfOutlined />}
+                      size="middle"
+                      onClick={() => window.open(emailDocumentUrl)}
+                      className={styles.filenameButton}
+                    >
+                      {emailDocumentUrl.split('_').slice(-1)[0]}
+                    </Button>
+                    <Tooltip title="Remove this file">
+                      <Button
+                        danger
+                        type="text"
+                        size="middle"
+                        icon={<CloseCircleOutlined />}
+                        onClick={() => setEmailDocumentUrl(null)}
+                      />
+                    </Tooltip>
+                  </Col>
+                )}
+              </Row>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row justify="end" align="center" gutter={16}>
+          <Col xs={12} md={6}>
+            <Button block type="default" onClick={() => closeModal()} loading={submitting}>
+              Cancel
+            </Button>
+          </Col>
+          <Col xs={12} md={6}>
+            <Button block type="primary" htmlType="submit" loading={submitting}>
+              Send Email
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  );
+};
+
+export default SendCustomerEmailModal;
