722ea1c2793ee0773e4aafe8634fa983e9c1831a
Ellianto <ellianto@10xers.co>
2021-03-22 23:08:18 +0700

    Implemented Send Email to Video Customers

--- src/pages/CreatorDashboard/Videos/index.js
+++ src/pages/CreatorDashboard/Videos/index.js
@@ -12,6 +12,7 @@ import {
   CheckCircleTwoTone,
   BookTwoTone,
   DeleteOutlined,
+  MailOutlined,
 } from '@ant-design/icons';
 
 import apis from 'apis';
@@ -26,6 +27,8 @@ import UploadVideoModal from 'components/UploadVideoModal';
 import { showErrorModal, showSuccessModal } from 'components/Modals/modals';
 import { isAPISuccess, generateUrlFromUsername, copyPageLinkToClipboard } from 'utils/helper';
 
+import { useGlobalContext } from 'services/globalContext';
+
 import styles from './styles.module.scss';
 
 const { Title, Text } = Typography;
@@ -35,6 +38,8 @@ const {
 } = dateUtil;
 
 const Videos = () => {
+  const { showSendEmailPopup } = useGlobalContext();
+
   const [selectedVideo, setSelectedVideo] = useState(null);
   const [videos, setVideos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -42,6 +47,73 @@ const Videos = () => {
   const [createModalVisible, setCreateModalVisible] = useState(false);
   const [formPart, setFormPart] = useState(1);
   const [shouldCloneVideo, setShouldCloneVideo] = useState(false);
+
+  const showEmailModal = (video) => {
+    let activeRecipients = [];
+    let expiredRecipients = [];
+    let userIdMap = new Map();
+
+    if (video.buyers && video.buyers?.length > 0) {
+      // Since video can be repeatedly bought by the same user
+      // (after it expires), we put checks here
+
+      video.buyers.forEach((buyer) => {
+        if (!userIdMap.has(buyer.external_id)) {
+          let isActive = !buyer.expired;
+
+          if (!buyer.expired) {
+            const foundBuyer = activeRecipients.find((recipient) => recipient.external_id === buyer.external_id);
+
+            if (!foundBuyer) {
+              activeRecipients.push(buyer);
+            }
+          } else {
+            const foundBuyer = expiredRecipients.find((recipient) => recipient.external_id === buyer.external_id);
+
+            if (!foundBuyer) {
+              expiredRecipients.push(buyer);
+            }
+          }
+
+          userIdMap.set(buyer.external_id, {
+            ...buyer,
+            isActive,
+          });
+        } else {
+          const mappedBuyer = userIdMap.get(buyer.external_id);
+
+          // If the user in the map is already active user
+          // that means it's the most up to date data
+          if (mappedBuyer.isActive) {
+            return;
+          }
+
+          // If the user in the map is expired user
+          // We check if the current data is an active
+          // if it is we update the data in the map
+          if (!buyer.expired) {
+            userIdMap.set(buyer.external_id, {
+              ...buyer,
+              isActive: true,
+            });
+
+            // Move the buyer data from expired to active array
+            activeRecipients.push(buyer);
+            expiredRecipients = expiredRecipients.filter((recipient) => recipient.external_id !== buyer.external_id);
+          }
+        }
+      });
+    }
+
+    showSendEmailPopup({
+      recipients: {
+        active: activeRecipients,
+        expired: expiredRecipients,
+      },
+      productId: video?.external_id || null,
+      productType: 'VIDEO',
+    });
+  };
 
   const showUploadVideoModal = (video = null, screen = 1) => {
     if (video !== null) {
@@ -238,6 +310,11 @@ const Videos = () => {
       align: 'right',
       render: (text, record) => (
         <Row gutter={8}>
+          <Col xs={24} md={2}>
+            <Tooltip title="Send Customer Email">
+              <Button type="text" onClick={() => showEmailModal(record)} icon={<MailOutlined />} />
+            </Tooltip>
+          </Col>
           <Col xs={24} md={2}>
             <Tooltip title="Edit">
               <Button
