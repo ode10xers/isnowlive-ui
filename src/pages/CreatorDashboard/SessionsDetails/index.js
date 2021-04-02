cc37d0096d85049315acc201aa468ea98ea58cfb
Ellianto <ellianto@10xers.co>
2021-04-02 13:09:12 +0700

    Fixed incorrect helper method name

--- src/pages/CreatorDashboard/SessionsDetails/index.js
+++ src/pages/CreatorDashboard/SessionsDetails/index.js
@@ -16,7 +16,7 @@ import ReactHtmlParser from 'react-html-parser';
 
 import apis from 'apis';
 import dateUtil from 'utils/date';
-import { generateUrlFromUsername, isAPISuccess, getDuration, copyPageLinkToClipboard } from 'utils/helper';
+import { generateUrlFromUsername, isAPISuccess, getDuration, copyToClipboard } from 'utils/helper';
 import { getLocalUserDetails } from 'utils/storage';
 
 import FileUpload from 'components/FileUpload';
@@ -173,11 +173,7 @@ const SessionsDetails = ({ match }) => {
               </Button>
             </Col>
             <Col xs={24} md={7} lg={5} xl={4}>
-              <Button
-                className={styles.headButton}
-                icon={<CopyOutlined />}
-                onClick={() => copyPageLinkToClipboard(publicUrl)}
-              >
+              <Button className={styles.headButton} icon={<CopyOutlined />} onClick={() => copyToClipboard(publicUrl)}>
                 Copy Page Link
               </Button>
             </Col>
