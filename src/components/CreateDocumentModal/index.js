33ec0a33a715e8faeb04f0da01c4deb0258c8d99
Ellianto <ellianto@10xers.co>
2021-04-04 15:58:05 +0700

    UI Implementation for Create Document Modal

--- src/components/CreateDocumentModal/index.js
+++ src/components/CreateDocumentModal/index.js
@@ -1,9 +1,139 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Row, Col, Modal, Form, Button, Typography, Input, Tooltip } from 'antd';
+import { FilePdfOutlined, CloseCircleOutlined } from '@ant-design/icons';
+
+import Loader from 'components/Loader';
+import FileUpload from 'components/FileUpload';
+
+import validationRules from 'utils/validation';
+
+import { profileFormItemLayout } from 'layouts/FormLayouts';
 
 import styles from './styles.module.scss';
+import { showErrorModal } from 'components/Modals/modals';
 
-const CreateDocumentModal = () => {
-  return <div></div>;
+// TODO: Modify and adjust when edit is available
+const CreateDocumentModal = ({ visible, closeModal }) => {
+  const [form] = Form.useForm();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [fileUrl, setFileUrl] = useState(null);
+
+  useEffect(() => {
+    if (!visible) {
+      form.resetFields();
+      setFileUrl(null);
+      setIsLoading(false);
+    }
+  }, [visible, form]);
+
+  const normFile = (e) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
+
+  const handleFileUrlChange = (fileUrl) => {
+    setFileUrl(fileUrl);
+    form.validateFields(['file_url']);
+  };
+
+  const handleSubmit = (values) => {
+    if (!fileUrl) {
+      showErrorModal('Please upload the file before submitting!');
+      return;
+    }
+
+    setIsLoading(true);
+
+    console.log(values);
+
+    setIsLoading(false);
+  };
+
+  return (
+    <Modal
+      title="Upload New Document"
+      centered={true}
+      visible={visible}
+      footer={null}
+      onCancel={() => closeModal(false)}
+      width={720}
+    >
+      <Loader size="large" loading={isLoading}>
+        <Form {...profileFormItemLayout} form={form} scrollToFirstError={true} onFinish={handleSubmit}>
+          <Row gutter={[8, 16]}>
+            <Col xs={24}>
+              <Form.Item name="file_name" label="File Name" rules={validationRules.requiredValidation}>
+                <Input placeholder="Enter filename" />
+              </Form.Item>
+            </Col>
+            <Col xs={24}>
+              <Form.Item
+                name="file_url"
+                label="Upload File"
+                valuePropName="fileList"
+                getValueFromEvent={normFile}
+                required={true}
+                rules={[
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      return fileUrl ? Promise.resolve() : Promise.reject(new Error('Please upload file'));
+                    },
+                  }),
+                ]}
+              >
+                <Row>
+                  <Col>
+                    <FileUpload name="file_url" value={fileUrl} onChange={handleFileUrlChange} listType="text" />
+                  </Col>
+                  {fileUrl && (
+                    <Col>
+                      <Button
+                        type="text"
+                        icon={<FilePdfOutlined />}
+                        size="middle"
+                        onClick={() => window.open(fileUrl)}
+                        className={styles.filenameButton}
+                      >
+                        {fileUrl.split('_').slice(-1)[0]}
+                      </Button>
+                      <Tooltip title="Remove this file">
+                        <Button
+                          danger
+                          type="text"
+                          size="middle"
+                          icon={<CloseCircleOutlined />}
+                          onClick={() => {
+                            setFileUrl(null);
+                            form.validateFields(['file_url']);
+                          }}
+                        />
+                      </Tooltip>
+                    </Col>
+                  )}
+                </Row>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row className={styles.modalActionRow} gutter={10} justify="end">
+            <Col xs={12} md={8} lg={6}>
+              <Button block type="default" onClick={() => closeModal(false)} loading={isLoading}>
+                Cancel
+              </Button>
+            </Col>
+            <Col xs={12} md={8} lg={6}>
+              <Button block type="primary" htmlType="submit" loading={isLoading}>
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Loader>
+    </Modal>
+  );
 };
 
 export default CreateDocumentModal;
--- src/pages/CreatorDashboard/Documents/index.js
+++ src/pages/CreatorDashboard/Documents/index.js
@@ -1,13 +1,153 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
-import { Row, Col, Button, Typography } from 'antd';
+import { Row, Col, Button, Typography, Tooltip, Card, Empty } from 'antd';
+import { DownloadOutlined, PlusCircleOutlined } from '@ant-design/icons';
 
 import Loader from 'components/Loader';
 import Table from 'components/Table';
 import CreateDocumentModal from 'components/CreateDocumentModal';
+import { showErrorModal } from 'components/Modals/modals';
+
+import { isAPISuccess } from 'utils/helper';
+import { isMobileDevice } from 'utils/device';
+
+import styles from './styles.module.scss';
+
+const { Title, Text } = Typography;
 
 const Documents = () => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [documents, setDocuments] = useState([]);
+  const [createDocumentModalVisible, setCreateDocumentModalVisible] = useState(false);
+
+  const getCreatorDocuments = useCallback(() => {
+    setIsLoading(true);
+    try {
+      // TODO: Implement API Here
+      const { status, data } = {
+        status: 200,
+        data: [
+          {
+            id: 'scooby-dooby-doo',
+            name: 'Dummy document',
+            url: '',
+          },
+          {
+            id: 'yada-daby-doo',
+            name: 'Testing document',
+            url: '',
+          },
+          {
+            id: 'scooby-daba-doo',
+            name: 'Just Some document',
+            url: '',
+          },
+        ],
+      };
+
+      if (isAPISuccess(status) && data) {
+        setDocuments(data);
+      }
+    } catch (error) {
+      showErrorModal('Failed to fetch user documents', error?.response?.data?.message || 'Something went wrong.');
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getCreatorDocuments();
+  }, [getCreatorDocuments]);
+
+  const showCreateDocumentModal = () => {
+    setCreateDocumentModalVisible(true);
+  };
+
+  const hideCreateDocumentModal = (shouldRefresh = false) => {
+    setCreateDocumentModalVisible(false);
+
+    if (shouldRefresh) {
+      getCreatorDocuments();
+    }
+  };
+
+  // TODO: Add other buttons/columns as necessary
+  const documentsColumns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: '80%',
+    },
+    {
+      title: 'Actions',
+      width: '20%',
+      render: (record) => (
+        <Row gutter={[8, 8]} justify="end">
+          <Col xs={24}>
+            <Tooltip title="Download document">
+              <Button type="link" icon={<DownloadOutlined />} onClick={() => window.open(record.url)} />
+            </Tooltip>
+          </Col>
+        </Row>
+      ),
+    },
+  ];
+
+  // TODO: Also prepare mobile UI
+
+  const renderMobileDocumentItems = (document) => {
+    const layout = (label, value) => (
+      <Row>
+        <Col span={9}>
+          <Text strong>{label}</Text>
+        </Col>
+        <Col span={15}>: {value}</Col>
+      </Row>
+    );
+
+    const downloadButton = (
+      <Tooltip title="Download document">
+        <Button type="link" icon={<DownloadOutlined />} onClick={() => window.open(document.url)} />
+      </Tooltip>
+    );
+
+    return (
+      <Col xs={24} key={document.id}>
+        <Card title={<Title level={5}> {document.name} </Title>}>{layout('Download', downloadButton)}</Card>
+      </Col>
+    );
+  };
+
+  return (
+    <div className={styles.box}>
+      <CreateDocumentModal visible={createDocumentModalVisible} closeModal={hideCreateDocumentModal} />
+      <Row gutter={[8, 16]}>
+        <Col xs={24} md={12} lg={16}>
+          <Title level={4}> Documents </Title>
+        </Col>
+        <Col xs={24} md={12} lg={8}>
+          <Button block type="primary" onClick={() => showCreateDocumentModal()} icon={<PlusCircleOutlined />}>
+            Upload new document
+          </Button>
+        </Col>
+        <Col xs={24}>
+          {isMobileDevice ? (
+            documents.length > 0 ? (
+              <Loader loading={isLoading} text="Fetching document details...">
+                <Row gutter={[16, 16]} justify="center">
+                  {documents.map(renderMobileDocumentItems)}
+                </Row>
+              </Loader>
+            ) : (
+              <Empty description="No documents found" />
+            )
+          ) : (
+            <Table columns={documentsColumns} data={documents} rowKey={(record) => record.id} loading={isLoading} />
+          )}
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Documents;
