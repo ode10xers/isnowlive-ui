058885743a19d26055599bb42d2a8ca309a7a7bc
Ellianto <ellianto@10xers.co>
2021-04-08 15:19:29 +0700

    Added dev filter for GTM

--- src/services/integrations/googleTagManager.js
+++ src/services/integrations/googleTagManager.js
@@ -1,12 +1,6 @@
-// const clearCustomVariableDataLayer = () => {
-//   window.dataLayer.push(gtmResetObject);
-// };
-
 export const pushToDataLayer = (eventName, eventData = {}) => {
   window.dataLayer.push({
     event: eventName,
     ...eventData,
   });
-
-  // clearCustomVariableDataLayer();
 };
