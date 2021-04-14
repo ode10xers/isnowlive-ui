e842c90d552e26d808b41d078ecd2911488f1827
Ellianto <ellianto@10xers.co>
2021-04-14 13:34:14 +0700

    [LIVE-397] Improved Zoom oAuth Link Flow

--- src/pages/LiveStream/index.js
+++ src/pages/LiveStream/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import { Form, Typography, Button, Space, Row, Col, Input, message } from 'antd';
+import { Form, Typography, Button, Space, Row, Col, Input, Modal, message } from 'antd';
 import { VideoCameraOutlined } from '@ant-design/icons';
 
 import OnboardSteps from 'components/OnboardSteps';
@@ -64,15 +64,23 @@ const LiveStream = () => {
           const localUserDetails = getLocalUserDetails();
           localUserDetails.zoom_connected = ZoomAuthType.OAUTH;
           localStorage.setItem('user-details', JSON.stringify(localUserDetails));
-          message.success('Zoom successfully setup!');
+          // message.success('Zoom successfully setup!');
+          Modal.success({
+            centered: true,
+            okText: 'Great',
+            title: 'Zoom account successfully connected',
+            content: `We'll take care of creating and managing unique zoom meetings for all your sessions. So you can focus on what you love doing.`,
+            onOk: () => history.push(Routes.creatorDashboard.rootPath),
+            closable: true,
+          });
           // setTimeout is used for better user experince suggest by Rahul
-          setTimeout(() => {
-            // if (isOnboarding) {
-            //   history.push(Routes.session);
-            // } else {
-            history.push(Routes.creatorDashboard.rootPath);
-            // }
-          }, 2000);
+          // setTimeout(() => {
+          // if (isOnboarding) {
+          //   history.push(Routes.session);
+          // } else {
+          //   history.push(Routes.creatorDashboard.rootPath);
+          // }
+          // }, 2000);
         }
       } catch (error) {
         message.error(error.response?.data?.message || 'Something went wrong.');
