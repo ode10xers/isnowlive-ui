1914ab6cdae06a4b175aa083dd146df91a28a4d4
Gopal Yadav <yadav.gopal43@gmail.com>
2021-04-17 12:23:55 +0530

    LIVE-334-embeddable-products

--- src/components/EmbeddableComponents/Videos/index.js
+++ src/components/EmbeddableComponents/Videos/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Loader from 'components/Loader';
 import PublicVideoList from 'components/PublicVideoList';
