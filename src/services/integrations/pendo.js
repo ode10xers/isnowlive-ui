7a80899e29c7707f12915d329769a67a43205688
Ellianto <ellianto@10xers.co>
2021-04-20 23:22:16 +0700

    [LIVE-417] Added previously removed account id mapping

--- src/services/integrations/pendo.js
+++ src/services/integrations/pendo.js
@@ -5,36 +5,40 @@ export const mapUserToPendo = (userDetails) => {
   const allowedStageEmail = 'felicia.karissa7';
 
   // These checks below will selectively initializes pendo
-  if (isDev && userDetails && userDetails.email.startsWith(allowedStageEmail)) {
-    // For Dev environments, only initializes for specific emails
-    console.log('Pendo Initialized on Stage Environment for ' + userDetails.email);
+  if (userDetails) {
+    if (isDev) {
+      // For Dev environments, only initializes for specific emails
 
-    window.pendo.initialize({
-      visitor: {
-        id: userDetails.external_id, // Required if user is logged in
-        email: userDetails.email, // Recommended if using Pendo Feedback, or NPS Email
-        //full_name:                  // Recommended if using Pendo Feedback
-        // role:         // Optional
+      if (userDetails.email.startsWith(allowedStageEmail)) {
+        console.log('Pendo Initialized on Stage Environment for ' + userDetails.email);
 
-        // You can add any additional visitor level key-values here,
-        // as long as it's not one of the above reserved names.
-      },
+        window.pendo.initialize({
+          visitor: {
+            id: userDetails.external_id, // Required if user is logged in
+            email: userDetails.email, // Recommended if using Pendo Feedback, or NPS Email
+            //full_name:                  // Recommended if using Pendo Feedback
+            // role:         // Optional
 
-      account: {
-        // id: userDetails.external_id, // Highly recommended
-        // name:         // Optional
-        // is_paying:    // Recommended if using Pendo Feedback
-        // monthly_value:// Recommended if using Pendo Feedback
-        // planLevel:    // Optional
-        // planPrice:    // Optional
-        // creationDate: // Optional
-        // You can add any additional account level key-values here,
-        // as long as it's not one of the above reserved names.
-      },
-    });
-  } else {
-    // Apply checks here
+            // You can add any additional visitor level key-values here,
+            // as long as it's not one of the above reserved names.
+          },
 
-    console.log('Apply checks first for PROD environment');
+          account: {
+            id: userDetails.external_id, // Highly recommended
+            // name:         // Optional
+            // is_paying:    // Recommended if using Pendo Feedback
+            // monthly_value:// Recommended if using Pendo Feedback
+            // planLevel:    // Optional
+            // planPrice:    // Optional
+            // creationDate: // Optional
+            // You can add any additional account level key-values here,
+            // as long as it's not one of the above reserved names.
+          },
+        });
+      }
+    } else {
+      // Apply checks here
+      console.log('Apply checks first for PROD environment');
+    }
   }
 };
