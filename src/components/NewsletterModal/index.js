04b3b8defceea177b05625b191efd6038ca47540
Abhijeet C <abhijeet.choubey@seenit.in>
2021-05-08 23:54:33 +0530

    newsletter dev done

--- src/components/NewsletterModal/index.js
+++ src/components/NewsletterModal/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Row, Col, Form, Input, Modal, Button, Space } from 'antd';
+import { Row, Col, Form, Typography, Input, Modal, Button } from 'antd';
 import Loader from 'components/Loader';
 import { showErrorModal, resetBodyStyle } from 'components/Modals/modals';
 import apis from 'apis';
 import { isAPISuccess } from 'utils/helper';
+import styles from './style.module.scss';
+const { Paragraph, Title } = Typography;
 
 const NewsletterModal = ({ visible, closeModal, isLoading }) => {
   const onFinish = async (values) => {
@@ -19,9 +21,10 @@ const NewsletterModal = ({ visible, closeModal, isLoading }) => {
 
       if (isAPISuccess(status)) {
         console.log('success');
+        closeModal(true);
       }
     } catch (error) {
-      showErrorModal('Failed to send emails', error?.respoonse?.data?.message || 'Something went wrong');
+      showErrorModal('Error', error?.respoonse?.data?.message || 'Something went wrong, Please try again');
     }
   };
 
@@ -35,35 +38,43 @@ const NewsletterModal = ({ visible, closeModal, isLoading }) => {
         afterClose={resetBodyStyle}
       >
         <Loader loading={isLoading} size="large">
-          <Form name="complex-form" onFinish={onFinish} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-            <Form.Item label="Name" style={{ marginBottom: 0 }}>
-              <Form.Item
-                name="firstName"
-                rules={[{ required: true }]}
-                style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
-              >
-                <Input placeholder="First" />
-              </Form.Item>
-              <Form.Item
-                name="lastName"
-                rules={[{ required: true }]}
-                style={{ display: 'inline-block', width: 'calc(50% - 8px)', margin: '0 8px' }}
-              >
-                <Input placeholder="Last1" />
-              </Form.Item>
-            </Form.Item>
-            <Form.Item label="Email">
-              <Space>
-                <Form.Item name="email" noStyle rules={[{ type: 'email' }]}>
-                  <Input placeholder="Please Enter your Email" />
+          <Form labelAlign="left" onFinish={onFinish} scrollToFirstError={true}>
+            <Row gutter={8}>
+              <Col xs={24}>
+                <Paragraph className={styles.textAlignCenter}>
+                  <Title level={4}>{`Subscribe`}</Title>
+                  {`Subscribe to my newsletter`}
+                </Paragraph>
+              </Col>
+              <Col xs={24} md={{ span: 18, offset: 3 }}>
+                <Form.Item label="Name" style={{ marginBottom: 0 }}>
+                  <Form.Item
+                    name="firstName"
+                    rules={[{ required: true }]}
+                    style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
+                  >
+                    <Input placeholder="First" />
+                  </Form.Item>
+                  <Form.Item
+                    name="lastName"
+                    rules={[{ required: true }]}
+                    style={{ display: 'inline-block', width: 'calc(50% - 8px)', margin: '0 8px' }}
+                  >
+                    <Input placeholder="Last" />
+                  </Form.Item>
                 </Form.Item>
-              </Space>
-            </Form.Item>
-            <Form.Item label=" " colon={false}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
+                <Form.Item label="Email">
+                  <Form.Item name="email" rules={[{ type: 'email' }]}>
+                    <Input placeholder="Please Enter your Email" />
+                  </Form.Item>
+                </Form.Item>
+                <Form.Item label=" " colon={false}>
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Row>
           </Form>
         </Loader>
       </Modal>
