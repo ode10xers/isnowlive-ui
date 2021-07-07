abbb81f3c4b9f202e9f6a8fc7fc4c9c36aada1a0
Ellianto <ellianto@10xers.co>
2021-07-07 20:36:27 +0700

    [LIVE-521] Fixed Session List component not taking full width

--- src/components/DynamicProfileComponents/SessionsProfileComponent/SessionListView/index.js
+++ src/components/DynamicProfileComponents/SessionsProfileComponent/SessionListView/index.js
@@ -64,7 +64,7 @@ const SessionListView = ({ limit = 2, sessions = [] }) => {
 
   const renderSessionDateList = (sessionDateData) => (
     <Col xs={24} key={sessionDateData.key}>
-      <Space direction="vertical">
+      <Space direction="vertical" className={styles.w100}>
         <Title level={4} className={styles.sessionDateSeparator}>
           {sessionDateData.title}
         </Title>
