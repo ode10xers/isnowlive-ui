eca25f7ba5d110dceaac5026a1d713672b1714d7
Abhijeet C <abhijeet@10xers.co>
2021-07-14 00:05:14 +0530

    added video on click function

--- src/utils/course.js
+++ src/utils/course.js
@@ -1,13 +1,13 @@
 export const getCourseSessionContentCount = (courseModules = []) =>
   courseModules.reduce(
     (acc, module) =>
-      (acc += module.module_content.filter((content) => content.product_type.toUpperCase() === 'SESSION').length ?? 0),
+      (acc += module.contents.filter((content) => content.product_type.toUpperCase() === 'SESSION').length ?? 0),
     0
   );
 
 export const getCourseVideoContentCount = (courseModules = []) =>
   courseModules.reduce(
     (acc, module) =>
-      (acc += module.module_content.filter((content) => content.product_type.toUpperCase() === 'VIDEO').length ?? 0),
+      (acc += module.contents.filter((content) => content.product_type.toUpperCase() === 'VIDEO').length ?? 0),
     0
   );
