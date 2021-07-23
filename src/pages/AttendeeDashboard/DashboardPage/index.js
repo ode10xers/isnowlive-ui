56f8adaf9fc3423d4a7960a6b7ef5dd8f086a4a5
Ellianto <ellianto@10xers.co>
2021-07-23 17:36:55 +0700

    [LIVE-540] Improved Sessions UI

--- src/pages/AttendeeDashboard/DashboardPage/index.js
+++ src/pages/AttendeeDashboard/DashboardPage/index.js
@@ -1,22 +1,33 @@
 import React, { useState, useCallback, useEffect } from 'react';
+import moment from 'moment';
 
-import { Row, Col, Typography, Spin, Image, Empty, Space, Button, message } from 'antd';
+import { Row, Col, Typography, Spin, Image, Empty, Space, Button, Modal, Popover, Popconfirm, message } from 'antd';
 import { BookTwoTone } from '@ant-design/icons';
 
 import apis from 'apis';
 import Routes from 'routes';
 
-import dateUtil from 'utils/date';
-
 import Table from 'components/Table';
 import { showErrorModal } from 'components/Modals/modals';
+import AddToCalendarButton from 'components/AddToCalendarButton';
 
-import { getDuration, isAPISuccess, isUnapprovedUserError, preventDefaults } from 'utils/helper';
+import dateUtil from 'utils/date';
+import { isInIframeWidget } from 'utils/widgets';
+import { redirectToInventoryPage } from 'utils/redirect';
+import {
+  getDuration,
+  isAPISuccess,
+  isUnapprovedUserError,
+  generateQueryString,
+  preventDefaults,
+  generateUrlFromUsername,
+} from 'utils/helper';
 
 import styles from './styles.module.scss';
 
 const {
-  formatDate: { toLocaleTime, toLongDateWithDay, toLocaleDate, toLongDateWithDayTime },
+  formatDate: { toLocaleTime, toLongDateWithDay, toLongDateWithDayTime },
+  timeCalculation: { isBeforeDate, isBeforeLimitHours },
 } = dateUtil;
 
 const { Text, Title } = Typography;
@@ -26,7 +37,6 @@ const whiteColor = '#ffffff';
 const DashboardPage = ({ match, history }) => {
   const [isSessionLoading, setIsSessionLoading] = useState(false);
   const [upcomingSessions, setUpcomingSessions] = useState([]);
-  const [filteredByDateSessions, setFilteredByDateSessions] = useState([]);
 
   const [isVideoLoading, setIsVideoLoading] = useState(false);
   const [videos, setVideos] = useState([]);
@@ -36,44 +46,13 @@ const DashboardPage = ({ match, history }) => {
   const fetchAttendeeUpcomingSessions = useCallback(async () => {
     setIsSessionLoading(true);
 
+    const sessionItemCount = 2;
+
     try {
       const { status, data } = await apis.session.getAttendeeUpcomingSession();
 
       if (isAPISuccess(status) && data) {
-        console.log('Session Data');
-        console.log(data.slice(0, 2));
-
-        let filterByDateSessions = [];
-        // NOTE: How much date to show in the UI
-        const dayLimit = 4;
-
-        data.forEach((session) => {
-          if (filterByDateSessions.length >= dayLimit) {
-            return;
-          }
-
-          const foundIndex = filterByDateSessions.findIndex(
-            (val) => val.start_time === toLocaleDate(session.start_time)
-          );
-
-          if (foundIndex >= 0) {
-            filterByDateSessions[foundIndex].children.push(session);
-          } else {
-            filterByDateSessions.push({
-              start_time: toLocaleDate(session.start_time),
-              name: session.start_time,
-              is_date: true,
-              children: [session],
-            });
-          }
-        });
-
-        setUpcomingSessions(data);
-        setFilteredByDateSessions(filterByDateSessions);
-
-        // if (filterByDateSessions.length > 0) {
-        //   setExpandedRowKeys([filterByDateSessions[0].start_time]);
-        // }
+        setUpcomingSessions(data.slice(0, sessionItemCount));
       }
     } catch (error) {
       if (!isUnapprovedUserError(error.response)) {
@@ -123,6 +102,109 @@ const DashboardPage = ({ match, history }) => {
     history.push(Routes.attendeeDashboard.rootPath + Routes.attendeeDashboard.videos);
   };
 
+  const openSessionInventoryDetails = (item) => {
+    redirectToInventoryPage(item);
+  };
+
+  const cancelOrderForSession = async (orderId) => {
+    try {
+      await apis.session.cancelCustomerOrder(orderId, { reason: 'requested_by_customer' });
+      Modal.success({
+        closable: true,
+        maskClosable: true,
+        title: 'Refund Successful',
+      });
+      fetchAttendeeUpcomingSessions();
+    } catch (error) {
+      if (!isUnapprovedUserError(error.response)) {
+        showErrorModal('Failed to cancel order', error?.response?.data?.message || 'Something went wrong');
+      }
+    }
+  };
+
+  const renderRefundPopup = (data) => {
+    if (data.is_refundable) {
+      if (isBeforeLimitHours(data.start_time, data.refund_before_hours)) {
+        return (
+          <Popconfirm
+            arrowPointAtCenter
+            placement="topRight"
+            title={
+              <Text>
+                Do you want to refund this session? <br />
+                You will get <strong>{` ${data.currency.toUpperCase()} ${data.refund_amount} `}</strong>
+                back.
+              </Text>
+            }
+            onConfirm={() => cancelOrderForSession(data.order_id)}
+            okText="Yes, Refund Session"
+            cancelText="No"
+          >
+            <Button block size="small" type="text" danger>
+              Cancel
+            </Button>
+          </Popconfirm>
+        );
+      } else {
+        return (
+          <Popover
+            arrowPointAtCenter
+            placement="topRight"
+            trigger="click"
+            title="Refund Time Limit Reached"
+            content={
+              <Text>
+                Sorry, as per the cancellation policy of <br />
+                this session,
+                <strong>
+                  it can only be cancelled <br />
+                  {data.refund_before_hours} hours
+                </strong>
+                before the session starts.
+              </Text>
+            }
+          >
+            <Button block size="small" type="text" danger>
+              Cancel
+            </Button>
+          </Popover>
+        );
+      }
+    } else {
+      return (
+        <Popover
+          arrowPointAtCenter
+          placement="topRight"
+          trigger="click"
+          title="Session Cannot be Refunded"
+          content={
+            <Text>
+              Sorry, this session is not refundable based <br />
+              on the creator's settings
+            </Text>
+          }
+        >
+          <Button block size="small" type="text" danger>
+            Cancel
+          </Button>
+        </Popover>
+      );
+    }
+  };
+
+  const rescheduleSession = (data) => {
+    const passedData = {
+      inventory_id: data.inventory_id,
+      order_id: data.order_id,
+      price: data.price,
+    };
+
+    window.open(
+      `${generateUrlFromUsername(data.username)}/reschedule?${generateQueryString(passedData)}`,
+      isInIframeWidget() ? '_self' : '_blank'
+    );
+  };
+
   //#endregion End of Business Logics
 
   //#region Start of Table Columns
@@ -141,8 +223,7 @@ const DashboardPage = ({ match, history }) => {
         },
         children: (
           <>
-            <Text className={styles.sessionNameWrapper}>{record.name}</Text>{' '}
-            {record.is_course ? <BookTwoTone twoToneColor="#1890ff" /> : null}
+            <Text>{record.name}</Text> {record.is_course ? <BookTwoTone twoToneColor="#1890ff" /> : null}
           </>
         ),
       }),
@@ -178,6 +259,76 @@ const DashboardPage = ({ match, history }) => {
         record?.start_time && record?.end_time
           ? `${toLocaleTime(record?.start_time)} - ${toLocaleTime(record?.end_time)}`
           : '-',
+    },
+    {
+      title: 'Actions',
+      // width: isPast ? '56px' : '360px',
+      render: (text, record) => {
+        return (
+          <Row justify="space-around">
+            <Col md={24} xl={6}>
+              <AddToCalendarButton
+                buttonText="Add to Cal"
+                eventData={{
+                  ...record,
+                  page_url: `${generateUrlFromUsername(record?.username)}/e/${record.inventory_id}`,
+                }}
+              />
+            </Col>
+
+            <Col md={24} lg={24} xl={4}>
+              {record.is_offline ? (
+                <Popover
+                  arrowPointAtCenter
+                  placement="topRight"
+                  trigger="click"
+                  title="Event Address"
+                  content={record.offline_event_address}
+                >
+                  <Button block size="small" type="text" className={styles.success}>
+                    In person
+                  </Button>
+                </Popover>
+              ) : (
+                <Button
+                  type="text"
+                  size="small"
+                  block
+                  className={
+                    !record.join_url || isBeforeDate(moment(record.start_time).subtract(15, 'minutes'))
+                      ? styles.disabledSuccess
+                      : styles.success
+                  }
+                  disabled={!record.join_url || isBeforeDate(moment(record.start_time).subtract(15, 'minutes'))}
+                  onClick={() => window.open(record.join_url)}
+                >
+                  Join
+                </Button>
+              )}
+            </Col>
+            <Col md={24} lg={24} xl={4}>
+              {renderRefundPopup(record)}
+            </Col>
+            <Col md={24} lg={24} xl={4}>
+              <Button block size="small" type="link" onClick={() => openSessionInventoryDetails(record)}>
+                Details
+              </Button>
+            </Col>
+
+            <Col md={24} lg={24} xl={6}>
+              <Button
+                block
+                size="small"
+                type="text"
+                className={styles.warning}
+                onClick={() => rescheduleSession(record)}
+              >
+                Reschedule
+              </Button>
+            </Col>
+          </Row>
+        );
+      },
     },
   ];
   //#endregion End of Table Columns
@@ -246,18 +397,18 @@ const DashboardPage = ({ match, history }) => {
         {/* Sessions Section */}
         <Col xs={24}>
           <Space direction="vertical" align="middle" className={styles.w100}>
-            <Title level={4}> Purchased Sessions </Title>
+            <Title level={4}> Your Purchased Sessions </Title>
             <div className={styles.sessionsContainer}>
               <Table
                 columns={sessionTableColumns}
-                data={upcomingSessions.slice(0, 4)}
+                data={upcomingSessions}
                 loading={isSessionLoading}
                 rowKey={(record) => record.inventory_external_id}
               />
             </div>
             <div className={styles.moreButtonContainer}>
               <Button type="primary" onClick={redirectToSessionsPage}>
-                More Sessions
+                See More Sessions Purchased By You
               </Button>
             </div>
           </Space>
@@ -265,7 +416,7 @@ const DashboardPage = ({ match, history }) => {
         {/* Videos Section */}
         <Col xs={24}>
           <Space direction="vertical">
-            <Title level={4}> Purchased Videos </Title>
+            <Title level={4}> Your Purchased Videos </Title>
             <div className={styles.videosContainer}>
               <Spin spinning={isVideoLoading} tip="Fetching video orders">
                 {videos.length > 0 ? (
@@ -275,7 +426,7 @@ const DashboardPage = ({ match, history }) => {
                       <Row justify="center">
                         <Col>
                           <Button type="primary" onClick={redirectToVideosPage}>
-                            More Videos
+                            See More Videos Purchased By You
                           </Button>
                         </Col>
                       </Row>
