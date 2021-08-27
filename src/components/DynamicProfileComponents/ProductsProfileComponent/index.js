1b16d5113f009f0902bacfe1d244e6f5e2d79f59
Ellianto <ellianto@10xers.co>
2021-08-27 14:30:50 +0700

    fixed old product UI showing inventory cards

--- src/components/DynamicProfileComponents/ProductsProfileComponent/index.js
+++ src/components/DynamicProfileComponents/ProductsProfileComponent/index.js
@@ -112,7 +112,54 @@ const ProductsProfileComponent = ({
       const { status, data } = await apis.user.getSessionsByUsername('upcoming');
 
       if (isAPISuccess(status) && data) {
-        setSessions(data);
+        // setSessions(data);
+        const upcomingInventories = data;
+        const sessionIds = Array.from(new Set(upcomingInventories.map((inventory) => inventory.session_id)));
+        const adjustedSessions = sessionIds
+          .map((sessionId) => upcomingInventories.filter((item) => item.session_id === sessionId))
+          .map((items) => ({
+            beginning: items[0].beginning,
+            color_code: items[0].color_code,
+            creator_username: items[0].creator_username,
+            currency: items[0].currency,
+            description: items[0].description,
+            document_urls: items[0].document_urls,
+            expiry: items[0].expiry,
+            group: items[0].group,
+            inventory: items.map((item) => ({
+              end_time: item.end_time,
+              inventory_external_id: item.inventory_external_id,
+              inventory_id: item.inventory_id,
+              is_published: item.is_published,
+              num_participants: item.num_participants,
+              offline_event_address: item.offline_event_address,
+              participants: item.participants,
+              session_date: item.session_date,
+              start_time: item.start_time,
+              start_url: item.start_url,
+            })),
+            is_active: items[0].is_active,
+            is_course: items[0].is_course,
+            is_offline: items[0].is_offline,
+            is_refundable: items[0].is_refundable,
+            max_participants: items[0].max_participants,
+            name: items[0].name,
+            offline_event_address: items[0].offline_event_address,
+            pay_what_you_want: items[0].pay_what_you_want,
+            prerequisites: items[0].prerequisites,
+            price: items[0].price,
+            recurring: items[0].recurring,
+            refund_before_hours: items[0].refund_before_hours,
+            session_external_id: items[0].session_external_id,
+            session_id: items[0].session_id,
+            session_image_url: items[0].session_image_url,
+            tags: items[0].tags,
+            total_price: items[0].total_price,
+            type: items[0].type,
+            user_timezone: items[0].user_timezone,
+            user_timezone_offset: items[0].user_timezone_offset,
+          }));
+        setSessions(adjustedSessions);
       }
     } catch (error) {
       console.error('Failed fetching sessions for creator');
