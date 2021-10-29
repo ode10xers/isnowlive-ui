d5e81d4855ddba366155e86de9058fab7a8c58f4
Ellianto <ellianto@10xers.co>
2021-10-29 15:49:56 +0700

    Adjusted UI for defaults

--- src/pages/PageBuilder/index.js
+++ src/pages/PageBuilder/index.js
@@ -41,6 +41,40 @@ const PageBuilder = ({ match, history }) => {
     // NOTE: Configuration object examples can be seen here
     // https://github.com/artf/grapesjs/blob/master/src/editor/config/config.js
 
+    const definedBlocks = [
+      {
+        id: 'section', // id is mandatory
+        label: '<b>Section</b>', // You can use HTML/SVG inside labels
+        attributes: { class: 'gjs-block-section' },
+        content: `<section>
+          <h1>This is a simple title</h1>
+          <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
+        </section>`,
+      },
+      {
+        id: 'text',
+        label: 'Text',
+        content: '<div data-gjs-type="text">Insert your text here</div>',
+      },
+      {
+        id: 'image',
+        label: 'Image',
+        // Select the component once it's dropped
+        select: true,
+        // You can pass components as a JSON instead of a simple HTML string,
+        // in this case we also use a defined component type `image`
+        content: { type: 'image' },
+        // This triggers `active` event on dropped components and the `image`
+        // reacts by opening the AssetManager
+        activate: true,
+      },
+      {
+        id: 'subscription-list',
+        label: 'Passion Subs List',
+        content: { type: 'subscription-list' },
+      },
+    ];
+
     const editor = grapesjs.init({
       // Indicate where to init the editor. You can also pass an HTMLElement
       container: '#builder-editor',
@@ -54,97 +88,65 @@ const PageBuilder = ({ match, history }) => {
       storageManager: false,
       keepUnusedStyles: false,
       // Avoid any default panel
-      panels: { defaults: [] },
+      // panels: { defaults: [] },
       blockManager: {
         appendTo: '#builder-blocks',
-        blocks: [
-          {
-            id: 'section', // id is mandatory
-            label: '<b>Section</b>', // You can use HTML/SVG inside labels
-            attributes: { class: 'gjs-block-section' },
-            content: `<section>
-              <h1>This is a simple title</h1>
-              <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
-            </section>`,
-          },
-          {
-            id: 'text',
-            label: 'Text',
-            content: '<div data-gjs-type="text">Insert your text here</div>',
-          },
-          {
-            id: 'image',
-            label: 'Image',
-            // Select the component once it's dropped
-            select: true,
-            // You can pass components as a JSON instead of a simple HTML string,
-            // in this case we also use a defined component type `image`
-            content: { type: 'image' },
-            // This triggers `active` event on dropped components and the `image`
-            // reacts by opening the AssetManager
-            activate: true,
-          },
-          {
-            id: 'subscription-list',
-            label: 'Passion Subs List',
-            content: { type: 'subscription-list' },
-          },
-        ],
+        blocks: definedBlocks,
       },
       plugins: [subscriptionListComponentType],
     });
 
     // Prevent links
-    editor.RichTextEditor.remove('link');
+    // editor.RichTextEditor.remove('link');
 
-    editor.Panels.addPanel({
-      id: 'panel-top',
-      el: '.panel__top',
-    });
-    editor.Panels.addPanel({
-      id: 'basic-actions',
-      el: '.panel__basic-actions',
-      buttons: [
-        {
-          id: 'visibility',
-          active: true, // active by default
-          className: 'btn-toggle-borders',
-          label: '<u>B</u>',
-          command: 'sw-visibility', // Built-in command
-        },
-        {
-          id: 'export',
-          className: 'btn-open-export',
-          label: 'Exp',
-          command: 'export-template',
-          context: 'export-template', // For grouping context of buttons from the same panel
-        },
-        {
-          id: 'show-json',
-          className: 'btn-show-json',
-          label: 'JSON',
-          context: 'show-json',
-          command(editorInstance) {
-            editorInstance.Modal.setTitle('Components JSON')
-              .setContent(
-                `<textarea style="width:100%; height: 250px;">
-                ${JSON.stringify(editor.getComponents())}
-              </textarea>`
-              )
-              .open();
-          },
-        },
-        {
-          id: 'export-html',
-          className: 'btn-export-html',
-          label: 'HTML',
-          context: 'export-html',
-          command(editorInstance) {
-            console.log(editorInstance.getHtml());
-          },
-        },
-      ],
-    });
+    // editor.Panels.addPanel({
+    //   id: 'panel-top',
+    //   el: '.panel__top',
+    // });
+    // editor.Panels.addPanel({
+    //   id: 'basic-actions',
+    //   el: '.panel__basic-actions',
+    //   buttons: [
+    //     {
+    //       id: 'visibility',
+    //       active: true, // active by default
+    //       className: 'btn-toggle-borders',
+    //       label: '<u>B</u>',
+    //       command: 'sw-visibility', // Built-in command
+    //     },
+    //     {
+    //       id: 'export',
+    //       className: 'btn-open-export',
+    //       label: 'Exp',
+    //       command: 'export-template',
+    //       context: 'export-template', // For grouping context of buttons from the same panel
+    //     },
+    //     {
+    //       id: 'show-json',
+    //       className: 'btn-show-json',
+    //       label: 'JSON',
+    //       context: 'show-json',
+    //       command(editorInstance) {
+    //         editorInstance.Modal.setTitle('Components JSON')
+    //           .setContent(
+    //             `<textarea style="width:100%; height: 250px;">
+    //             ${JSON.stringify(editor.getComponents())}
+    //           </textarea>`
+    //           )
+    //           .open();
+    //       },
+    //     },
+    //     {
+    //       id: 'export-html',
+    //       className: 'btn-export-html',
+    //       label: 'HTML',
+    //       context: 'export-html',
+    //       command(editorInstance) {
+    //         console.log(editorInstance.getHtml());
+    //       },
+    //     },
+    //   ],
+    // });
 
     setGjsEditor(editor);
   }, []);
