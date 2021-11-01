3244fe41a24e60b3c80400727ca45eb5f50b87eb
Ellianto <ellianto@10xers.co>
2021-11-01 15:19:57 +0700

    Improved UI with custom block + traits

--- src/pages/PageBuilder/index.js
+++ src/pages/PageBuilder/index.js
@@ -5,10 +5,21 @@ import 'grapesjs/dist/css/grapes.min.css';
 import grapesjs from 'grapesjs';
 
 import definedBlocks from './blocks.js';
-import stylePanelSectors from './style_panel.js';
+import definedPanels from './panels.js';
+// import stylePanelSectors from './style_panel.js';
 
 //eslint-disable-next-line
 import styles from './style.module.scss';
+
+const DummyTemplate = {
+  html:
+    '<div layout="left" class="text-section-container"><h1>Section Title</h1><p>Section Content</p></div><div layout="right" id="ibyq" class="text-section-container"><h1>Section Title</h1><p>Section Content</p></div><div layout="center" id="iwt8" class="text-section-container"><h1>Section Title</h1><p>Section Content</p></div>',
+  components:
+    '[{"type":"text-section","classes":["text-section-container"],"attributes":{"layout":"left"},"components":[{"tagName":"h1","type":"text","name":"Section Title","removable":false,"draggable":false,"badgable":false,"highlightable":false,"hoverable":false,"content":"Section Title"},{"tagName":"p","type":"text","name":"Section Content","removable":false,"draggable":false,"badgable":false,"highlightable":false,"hoverable":false,"content":"Section Content"}]},{"type":"text-section","classes":["text-section-container"],"attributes":{"layout":"right","id":"ibyq"},"components":[{"tagName":"h1","type":"text","name":"Section Title","removable":false,"draggable":false,"badgable":false,"highlightable":false,"hoverable":false,"content":"Section Title"},{"tagName":"p","type":"text","name":"Section Content","removable":false,"draggable":false,"badgable":false,"highlightable":false,"hoverable":false,"content":"Section Content"}]},{"type":"text-section","classes":["text-section-container"],"attributes":{"layout":"center","id":"iwt8"},"components":[{"tagName":"h1","type":"text","name":"Section Title","removable":false,"draggable":false,"badgable":false,"highlightable":false,"hoverable":false,"content":"Section Title"},{"tagName":"p","type":"text","name":"Section Content","removable":false,"draggable":false,"badgable":false,"highlightable":false,"hoverable":false,"content":"Section Content"}]}]',
+  css: '* { box-sizing: border-box; } body {margin: 0;}#ibyq{text-align:right;}#iwt8{text-align:center;}',
+  styles:
+    '[{"selectors":["#ibyq"],"style":{"text-align":"right"}},{"selectors":["#iwt8"],"style":{"text-align":"center"}}]',
+};
 
 const PageBuilder = ({ match, history }) => {
   const [gjsEditor, setGjsEditor] = useState(null);
@@ -17,78 +28,186 @@ const PageBuilder = ({ match, history }) => {
     // NOTE: Configuration object examples can be seen here
     // https://github.com/artf/grapesjs/blob/master/src/dom_components/model/Component.js
 
+    const TextSectionComponent = (editor) => {
+      // TODO: Modify this according to the requirements
+      editor.TraitManager.addType('text-section-layout', {
+        // Expects as return a simple HTML string or an HTML element
+        noLabel: true,
+        templateInput: `<div class="custom-trait-layout">
+        <div class="custom-trait-label">
+          Layout
+        </div>
+        <div class="custom-trait-input" data-input>
+        </div>
+      </div>`,
+        createInput({ trait }) {
+          // Here we can decide to use properties from the trait
+          const options = [
+            { id: 'left', name: 'Left' },
+            { id: 'right', name: 'Right' },
+            { id: 'center', name: 'Centered' },
+          ];
+
+          // Create a new element container and add some content
+          const el = document.createElement('div');
+          el.innerHTML = `
+          <select class="text-section-layout-select" style="color: 'black'">
+            ${options.map((opt) => `<option value="${opt.id}">${opt.name}</option>`).join('')}
+          </select>
+        `;
+
+          return el;
+        },
+        // Update the component based on element changes
+        // `elInput` is the result HTMLElement you get from `createInput`
+        onEvent({ elInput, component, event }) {
+          const inputType = elInput.querySelector('.text-section-layout-select');
+          component.setStyle({
+            'text-align': inputType.value ?? 'left',
+          });
+          component.addAttributes({
+            layout: inputType.value,
+            // style : `text-align:${inputType.value ?? 'left'}; padding: 20px; display:block;`
+          });
+        },
+        // Update elements on the component change
+        onUpdate({ elInput, component }) {
+          const layout = component.getAttributes()['layout'] || 'left';
+          const inputType = elInput.querySelector('.text-section-layout-select');
+          inputType.value = layout;
+
+          inputType.dispatchEvent(new CustomEvent('change'));
+        },
+      });
+
+      editor.DomComponents.addType('text-section', {
+        model: {
+          defaults: {
+            tagName: 'div',
+            name: 'Text Section',
+            droppable: false,
+            attributes: {
+              layout: 'left',
+              // style: `text-align:left; padding: 20px; display: block;`,
+              class: 'text-section-container',
+            },
+            traits: [
+              {
+                type: 'text-section-layout',
+                name: 'layout',
+              },
+            ],
+            // components: `
+            //   <h1 class="text-section-title">Section Title</h1>
+            //   <p class="text-section-content">You can place your section content here. Double click on this text to edit</p>
+            // `,
+            //? Above is the example of using default components with HTML strings
+            //? Below we try to customize the behavior
+            components: [
+              {
+                tagName: 'h1',
+                type: 'text',
+                content: 'Section Title',
+                name: 'Section Title',
+                attributes: {},
+                traits: [],
+                removable: false,
+                draggable: false,
+                badgable: false,
+                droppable: false,
+                highlightable: false,
+                editable: true,
+                hoverable: false,
+                toolbar: [],
+              },
+              {
+                tagName: 'p',
+                type: 'text',
+                content: 'Section Content',
+                name: 'Section Content',
+                attributes: {},
+                traits: [],
+                removable: false,
+                draggable: false,
+                badgable: false,
+                droppable: false,
+                highlightable: false,
+                editable: true,
+                hoverable: false,
+                toolbar: [],
+              },
+            ],
+            styles: `
+              .text-section-container {
+                display: block;
+                padding: 20px;
+              }
+            `,
+          },
+        },
+      });
+    };
+
     const editor = grapesjs.init({
       // Indicate where to init the editor. You can also pass an HTMLElement
       container: '#builder-editor',
       // Get the content for the canvas directly from the element
       // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
-      fromElement: true,
+      // fromElement: true,
       // Size of the editor
       height: '100vh',
       width: 'auto',
-      // Disable the storage manager for the moment
-      storageManager: false,
+      storageManager: {
+        id: 'gjs-', // Prefix identifier that will be used on parameters
+        type: 'local', // Type of the storage
+        autosave: true, // Store data automatically
+        autoload: false, // Autoload stored data on init
+        stepsBeforeSave: 1, // If autosave enabled, indicates how many changes are necessary before store method is triggered
+      },
       keepUnusedStyles: false,
       // Avoid any default panel
-      // panels: { defaults: [] },
+      panels: definedPanels,
       // Built-in props for styles
       // https://grapesjs.com/docs/modules/Style-manager.html#built-in-properties
-      styleManager: {
-        sectors: stylePanelSectors,
-      },
+      // styleManager: {
+      //   sectors: stylePanelSectors,
+      // },
       blockManager: {
         blocks: definedBlocks,
       },
+      deviceManager: {
+        devices: [
+          {
+            name: 'Desktop',
+            width: '', // default size
+          },
+          {
+            name: 'Tablet',
+            width: '768px', // this value will be used on canvas width
+            widthMedia: '768px', // this value will be used in CSS @media
+          },
+          {
+            name: 'Mobile',
+            width: '576px', // this value will be used on canvas width
+            widthMedia: '576px', // this value will be used in CSS @media
+          },
+        ],
+      },
+      plugins: [TextSectionComponent],
     });
 
-    // editor.Panels.addPanel({
-    //   id: 'panel-top',
-    //   el: '.panel__top',
-    // });
-    // editor.Panels.addPanel({
-    //   id: 'basic-actions',
-    //   el: '.panel__basic-actions',
-    //   buttons: [
-    //     {
-    //       id: 'visibility',
-    //       active: true, // active by default
-    //       className: 'btn-toggle-borders',
-    //       label: '<u>B</u>',
-    //       command: 'sw-visibility', // Built-in command
-    //     },
-    //     {
-    //       id: 'export',
-    //       className: 'btn-open-export',
-    //       label: 'Exp',
-    //       command: 'export-template',
-    //       context: 'export-template', // For grouping context of buttons from the same panel
-    //     },
-    //     {
-    //       id: 'show-json',
-    //       className: 'btn-show-json',
-    //       label: 'JSON',
-    //       context: 'show-json',
-    //       command(editorInstance) {
-    //         editorInstance.Modal.setTitle('Components JSON')
-    //           .setContent(
-    //             `<textarea style="width:100%; height: 250px;">
-    //             ${JSON.stringify(editor.getComponents())}
-    //           </textarea>`
-    //           )
-    //           .open();
-    //       },
-    //     },
-    //     {
-    //       id: 'export-html',
-    //       className: 'btn-export-html',
-    //       label: 'HTML',
-    //       context: 'export-html',
-    //       command(editorInstance) {
-    //         console.log(editorInstance.getHtml());
-    //       },
-    //     },
-    //   ],
-    // });
+    editor.Commands.add('set-device-desktop', {
+      run: (editor) => editor.setDevice('Desktop'),
+    });
+    editor.Commands.add('set-device-tablet', {
+      run: (editor) => editor.setDevice('Tablet'),
+    });
+    editor.Commands.add('set-device-mobile', {
+      run: (editor) => editor.setDevice('Mobile'),
+    });
+    editor.Commands.add('save-as-json', {
+      run: (editor) => console.log(JSON.stringify(editor.getComponents())),
+    });
 
     setGjsEditor(editor);
   }, []);
@@ -97,16 +216,17 @@ const PageBuilder = ({ match, history }) => {
     initializeGrapesJSEditor();
   }, [initializeGrapesJSEditor]);
 
+  // Test loading template
+  // useEffect(() => {
+  //   if (gjsEditor) {
+  //     console.log("Loading Template...");
+  //     setTimeout(() => gjsEditor.load(), 2000);
+  //   }
+  // }, [gjsEditor]);
+
   return (
     <div id="builder-page">
-      <div className="panel__top">
-        <div className="panel__basic-actions"></div>
-      </div>
-      <div id="builder-editor">
-        <h1> Hello World Component Here </h1>
-        <p> This is the default template for the editor canvas </p>
-      </div>
-      <div id="builder-blocks"></div>
+      <div id="builder-editor"></div>
     </div>
   );
 };
---
+++ src/pages/PageBuilder/panels.js
@@ -0,0 +1,138 @@
+// This is the default value
+// taken from https://github.com/artf/grapesjs/blob/master/src/panels/config/config.js
+
+// const swv = 'sw-visibility';
+const expt = 'export-template';
+// const osm = 'open-sm';
+const otm = 'open-tm';
+const ola = 'open-layers';
+const obl = 'open-blocks';
+const ful = 'fullscreen';
+const prv = 'preview';
+
+const sdm = 'set-device-mobile';
+const sdt = 'set-device-tablet';
+const sdd = 'set-device-desktop';
+
+const saj = 'save-as-json';
+
+// TODO: Add more commands later
+// Responsive select, export to JSON
+export default {
+  stylePrefix: 'pn-',
+
+  // Default panels fa-sliders for features
+  defaults: [
+    // {
+    //   id: 'commands',
+    //   buttons: [{}]
+    // },
+    {
+      id: 'devices',
+      buttons: [
+        {
+          active: true,
+          id: sdd,
+          className: 'fa fa-desktop',
+          command: sdd,
+          context: sdd,
+          attributes: { title: 'Set Desktop View' },
+        },
+        {
+          id: sdt,
+          className: 'fa fa-tablet',
+          command: sdt,
+          context: sdt,
+          attributes: { title: 'Set Tablet View' },
+        },
+        {
+          id: sdm,
+          className: 'fa fa-mobile',
+          command: sdm,
+          context: sdm,
+          attributes: { title: 'Set Mobile View' },
+        },
+      ],
+    },
+    {
+      id: 'options',
+      buttons: [
+        // {
+        //   active: true,
+        //   id: swv,
+        //   className: 'fa fa-square-o',
+        //   command: swv,
+        //   context: swv,
+        //   attributes: { title: 'View components' }
+        // },
+        {
+          id: prv,
+          className: 'fa fa-eye',
+          command: prv,
+          context: prv,
+          attributes: { title: 'Preview' },
+        },
+        {
+          id: ful,
+          className: 'fa fa-arrows-alt',
+          command: ful,
+          context: ful,
+          attributes: { title: 'Fullscreen' },
+        },
+        {
+          id: expt,
+          className: 'fa fa-code',
+          command: expt,
+          attributes: { title: 'View code' },
+        },
+        {
+          id: saj,
+          className: 'fa fa-floppy-o',
+          command: saj,
+          attributes: { title: 'Save Template' },
+        },
+      ],
+    },
+    {
+      id: 'views',
+      buttons: [
+        // {
+        //   id: osm,
+        //   className: 'fa fa-paint-brush',
+        //   command: osm,
+        //   active: true,
+        //   togglable: 0,
+        //   attributes: { title: 'Open Style Manager' }
+        // },
+        {
+          id: otm,
+          className: 'fa fa-cog',
+          command: otm,
+          togglable: 0,
+          attributes: { title: 'Settings' },
+        },
+        {
+          id: ola,
+          className: 'fa fa-bars',
+          command: ola,
+          togglable: 0,
+          attributes: { title: 'Open Layer Manager' },
+        },
+        {
+          id: obl,
+          active: true,
+          className: 'fa fa-th-large',
+          command: obl,
+          togglable: 0,
+          attributes: { title: 'Open Blocks' },
+        },
+      ],
+    },
+  ],
+
+  // Editor model
+  em: null,
+
+  // Delay before show children buttons (in milliseconds)
+  delayBtnsShow: 300,
+};
